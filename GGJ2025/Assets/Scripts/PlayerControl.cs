02667c83e000fa0312bdb7d7ea186e19e88ec831
MacaroniJam <joeljhenry28012004@gmail.com>
2025-01-25 03:09:38 -0500

    Added currents code and adjust player sprite

--- GGJ2025/Assets/Scripts/PlayerControl.cs
+++ GGJ2025/Assets/Scripts/PlayerControl.cs
@@ -4,18 +4,23 @@ using UnityEngine.Rendering.Universal;
 public class PlayerControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private int life = 2;
+    public int life = 2;
 
     private Rigidbody2D rb;
     public float moveSpeed;
     public float floatSpeed;
+    public float maxSpeed;
+    public float slowDownRate;
     private Vector2 movementInput;
+    private bool slowDown = false;
 
     public Transform lantern;
     public Light2D light2D;
 
     public Transform cam;
     public float delay;
+
+    public Transform circle;
 
     void Start()
     {
@@ -25,18 +30,53 @@ public class PlayerControl : MonoBehaviour
     }
     private void Update()
     {
-       
+        if (slowDown)
+        {
+            if (floatSpeed > 1)
+            {
+                //adjust the rate of slowing down
+                floatSpeed -= slowDownRate *Time.deltaTime;
+            }
+            else
+            {
+                slowDown = false;
+            }
+        }
+
+        if (floatSpeed < 1)
+        {
+            floatSpeed = 1;
+        }
+
+        
+
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         //lantern position = player position
         lantern.position = this.transform.position;
+        circle.position = this.transform.position;
 
         //camera y position = player y position delayed
-        if (this.transform.position.y > 0)
+        if (this.transform.position.y > 0 && floatSpeed == 1)
         {
-            cam.position = new Vector3(cam.position.x, this.transform.position.y, -10f);
+
+            float speed = 1f;
+
+            // Smoothly adjust the camera's Y position
+            float smoothTime = 0.3f; // Larger values = slower smoothing
+            float newY = Mathf.SmoothDamp(cam.position.y, this.transform.position.y, ref speed, smoothTime);
+
+            // Update camera position
+            cam.position = new Vector3(cam.position.x, newY, -10f);
+        }
+
+
+        if (floatSpeed > 1)
+        {
+            float newY = Mathf.Lerp(cam.position.y, this.transform.position.y - 2f, delay * Time.deltaTime);
+            cam.position = new Vector3(cam.position.x, newY, -10f);
         }
 
         
@@ -53,7 +93,7 @@ public class PlayerControl : MonoBehaviour
 
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
@@ -64,7 +104,24 @@ public class PlayerControl : MonoBehaviour
                 light2D.falloffIntensity = 0.8f;
             }
         }
-    }*/
+
+        if (collision.gameObject.tag == "Current")
+        {
+            if (floatSpeed <= 1)
+            {
+                floatSpeed = maxSpeed;
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Current")
+        {
+            slowDown = true;
+        }
+    }
 
 
 }
